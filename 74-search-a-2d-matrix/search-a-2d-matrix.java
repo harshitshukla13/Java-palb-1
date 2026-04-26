db6bf57ebe53ad655a6a5b44eb508cf1293d9df7
harshitshukla13 <shuklakplv100@gmail.com>
2026-04-26 15:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (63.08%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -6,8 +6,9 @@ class Solution {
         int low = 0, high = m * n - 1;
 
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
 
+            // convert 1D index → 2D indices
             int row = mid / n;
             int col = mid % n;
 
